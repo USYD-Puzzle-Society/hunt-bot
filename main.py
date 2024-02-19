d70a788357a63fb708170f7855efbcfe1cd3db5b
Adam Zhang <azha3852@uni.sydney.edu.au>
2024-02-19 18:08:22 +1100

    created basic bot

--- main.py
+++ main.py
@@ -1,2 +1,31 @@
 import discord
+from discord.ext import commands
 
+TOKEN = ""
+
+intents = discord.Intents.all()
+
+bot = commands.Bot(command_prefix='!', help_command=None, intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Connected as {bot.user}.")
+    
+    try:
+        synced = await bot.tree.sync()
+        print(f"Synced {len(synced)} commands.")
+    except Exception as e:
+        print(e)
+
+@bot.command()
+async def sync(ctx: commands.context.Context):
+    try:
+        synced = await bot.tree.sync()
+        print(f"Synced {len(synced)} commands.")
+    except Exception as e:
+        print(e)
+
+with open(".token", 'r') as t:
+    TOKEN = t.readline().strip()
+
+bot.run(TOKEN)
