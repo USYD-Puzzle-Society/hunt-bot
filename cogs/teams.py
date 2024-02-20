eb0f03f2036391a2fadd4cc0e7e86e6b8fc186d7
Hung Pham <skellybg245@gmail.com>
2024-02-20 18:58:08 +1100

    feat: deploy application on push to main branch (#4)

--- cogs/teams.py
+++ cogs/teams.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 from discord import app_commands
 from discord import Guild
 
+
 class Team(commands.GroupCog):
     def __init__(self, bot):
         self.bot = bot
@@ -16,13 +17,12 @@ class Team(commands.GroupCog):
         # include code to check that the name is not already taken
         # will involve a call to the respective sql function
 
-        
         # if name not taken, add check for profanity and such
 
         team_role = await Guild.create_role(guild, name=team_name)
         overwrites = {
             guild.default_role: discord.PermissionOverwrite(read_messages=False),
-            team_role: discord.PermissionOverwrite(read_messages=True)
+            team_role: discord.PermissionOverwrite(read_messages=True),
         }
         category = await Guild.create_category(guild, team_name, overwrites=overwrites)
         await category.create_text_channel(name=team_name)
@@ -30,14 +30,17 @@ class Team(commands.GroupCog):
 
         # give role to user
         await user.add_roles(team_role)
-        await interaction.response.send_message(f"Team \"{team_name}\" created successfully!")
+        await interaction.response.send_message(
+            f'Team "{team_name}" created successfully!'
+        )
 
     @app_commands.command(name="leave")
     async def leave_team(self, interaction: discord.Interaction, team_name: str):
         # remove team role from user
         # if there are no more people in the team, delete the role and channels
-        
+
         pass
+
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(Team(bot))
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from src.config import config
 import os
 
 TOKEN = ""
@@ -8,11 +9,13 @@ COGS_DIR = "cogs"
 
 intents = discord.Intents.all()
 
-bot = commands.Bot(command_prefix='!', help_command=None, intents=intents)
+bot = commands.Bot(command_prefix="!", help_command=None, intents=intents)
+
 
 @bot.event
 async def on_ready():
     print(f"Connected as {bot.user}.")
+
 
 # load all available cogs on startup
 @bot.command()
@@ -24,12 +27,14 @@ async def startup(ctx: commands.context.Context):
             print(f"Loaded {filename}")
     await ctx.send(f"Loaded all cogs")
 
+
 # command to load a cog
 @bot.command()
 @commands.has_role(EXEC_ID)
 async def load(ctx: commands.context.Context, extension):
     await bot.load_extension(f"{COGS_DIR}.{extension}")
     await ctx.send(f"Loaded {extension} cog")
+
 
 # command to unload a cog
 @bot.command()
@@ -38,6 +43,7 @@ async def unload(ctx: commands.context.Context, extension):
     await bot.unload_extension(f"{COGS_DIR}.{extension}")
     await ctx.send(f"Unloaded {extension} cog")
 
+
 # command to reload a cog
 @bot.command()
 @commands.has_role(EXEC_ID)
@@ -45,15 +51,15 @@ async def reload(ctx: commands.context.Context, extension):
     await bot.reload_extension(f"{COGS_DIR}.{extension}")
     await ctx.send(f"Reloaded {extension} cog")
 
+
 @bot.command()
 async def sync(ctx: commands.context.Context):
     try:
-        synced = await bot.tree.sync()
+        bot.tree.copy_global_to(guild=ctx.guild)
+        synced = await bot.tree.sync(guild=ctx.guild)
         await ctx.send(f"Synced {len(synced)} commands.")
     except Exception as e:
         print(e)
 
-with open(".token", 'r') as t:
-    TOKEN = t.readline().strip()
 
-bot.run(TOKEN)
+bot.run(config["DISCORD_TOKEN"])
