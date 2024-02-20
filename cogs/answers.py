a7f4e601a7fabc7a5c0a3fd1a1b3783aa8fdd7ef
SkellyBG <skellybg245@gmail.com>
2024-02-20 18:56:29 +1100

    chore: run format

--- cogs/answers.py
+++ cogs/answers.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 from discord import app_commands
 
+
 class Answers(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -13,14 +14,17 @@ class Answers(commands.Cog):
             "God's Kitchen": "what's the opposite of gordon ramsay",
             "Librinth": "books idk",
             "Susharks in the Wafer": "i'm kinda hungry",
-            "We Need to Eat": "aaaaaaaaanoodl"
+            "We Need to Eat": "aaaaaaaaanoodl",
         }
 
     @app_commands.command(name="check")
     # remember to add a check so that users can only check answers in their respective
     # team channels
-    async def check_answer(self, interaction: discord.Interaction, puzz_name: str, answer: str):
+    async def check_answer(
+        self, interaction: discord.Interaction, puzz_name: str, answer: str
+    ):
         pass
+
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(Answers(bot))
