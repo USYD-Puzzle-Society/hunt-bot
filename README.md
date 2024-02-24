a49ce7510df38b9645655128f0976e672eabba1a
Hung Pham <skellybg245@gmail.com>
2024-02-24 18:55:59 +1100

    feat: enable connection to database (#5)

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # hunt bot
- 
+
+## Local database setup
+
+Included in the repository is a dockercompose.yml file to help with setting up the database.
+To start the local database, simply do:
+
+    docker compose up postgres16
+
+This will start a local database that runs on port 5432 and store its data in a local pg_data directory.
+To connect to the docker database, do:
+
+    docker exec -it postgres16 psql -U postgres
+
+For future maintainer, since the infra we're curerntly using is Fly.io:
+To run the schema on remote fly machine, open a tunnel with `fly proxy`, then use `psql` with -f and -h flags (-h flags forces psql to use TCP/IP instead of UNIX sockets)
---
+++ src/db/schema.sql
@@ -0,0 +1,45 @@
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+DROP TABLE IF EXISTS public.teams;
+DROP TABLE IF EXISTS public.players;
+DROP TABLE IF EXISTS public.puzzles;
+DROP TABLE IF EXISTS public.submission;
+
+CREATE TABLE public.teams (
+    team_name text PRIMARY KEY,
+    category_channel_id text NOT NULL,
+    voice_channel_id text NOT NULL,
+    text_channel_id text NOT NULL,
+    team_role_id text NOT NULL,
+    puzzle_solved integer NOT NULL default 0
+);
+
+CREATE TABLE public.players (
+    discord_id text PRIMARY KEY,
+    team_name text NOT NULL REFERENCES public.teams ON DELETE CASCADE
+);
+
+CREATE TABLE public.puzzles (
+    puzzle_id text PRIMARY KEY,
+    puzzle_name text NOT NULL,
+    puzzle_answer text NOT NULL,
+    puzzle_author text NOT NULL,
+    uni text NOT NULL
+);
+
+CREATE TABLE public.submissions (
+    puzzle_id text NOT NULL REFERENCES public.puzzles ON DELETE CASCADE,    
+    team_name text NOT NULL REFERENCES public.teams ON DELETE CASCADE,
+    submission_time timestamptz NOT NULL,
+    submission_answer text NOT NULL,
+    submission_is_correct boolean NOT NULL
+);
