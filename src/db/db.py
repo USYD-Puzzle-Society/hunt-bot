d9ad0177ea4a6341490b67995104c99dfa825c76
SkellyBG <skellybg245@gmail.com>
2024-02-24 18:44:05 +1100

    fix: example

--- src/db/db.py
+++ src/db/db.py
@@ -11,12 +11,27 @@ async def truncate():
             await acur.execute("TRUNCATE public.teams CASCADE")
 
 
-async def create_team(team_name: str, team_channel_id: str):
+async def create_team(
+    team_name: str,
+    category_channel_id: str,
+    voice_channel_id: str,
+    text_channel_id: str,
+    team_role_id: str,
+):
     async with await psycopg.AsyncConnection.connect(DATABASE_URL) as aconn:
         async with aconn.cursor() as acur:
             await acur.execute(
-                "INSERT INTO public.teams (team_name, team_channel) VALUES (%s, %s)",
-                (team_name, team_channel_id),
+                """
+                INSERT INTO public.teams (team_name, category_channel_id, voice_channel_id, text_channel_id, team_role_id) 
+                VALUES (%s, %s, %s, %s, %s)
+                """,
+                (
+                    team_name,
+                    category_channel_id,
+                    voice_channel_id,
+                    text_channel_id,
+                    team_role_id,
+                ),
             )
 
 
@@ -33,7 +48,7 @@ async def get_team(team_name: str):
 
 async def main():
     await truncate()
-    await create_team("test", "boop")
+    await create_team("test", "boop1", "boop2", "boop3", "boop4")
     await get_team("test")
 
 
