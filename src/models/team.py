9005dcc9c8f5a6883639e67792e13a196214a6e6
Adam Zhang <azha3852@uni.sydney.edu.au>
2024-02-26 23:49:42 +1100

    Create Team dataclass

---
+++ src/models/team.py
@@ -0,0 +1,9 @@
+from dataclasses import dataclass
+
+@dataclass
+class Team:
+    team_name: str
+    category_channel_id: str
+    voice_channel_id: str
+    text_channel_id: str
+    team_role_id: str
