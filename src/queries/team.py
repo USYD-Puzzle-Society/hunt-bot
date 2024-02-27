5904f57136ec4430f1247366077c57f7bdb34c1f
Adam Zhang <azha3852@uni.sydney.edu.au>
2024-02-27 13:48:33 +1100

    feat: add remove_team function

--- src/queries/team.py
+++ src/queries/team.py
@@ -15,29 +15,28 @@ async def create_team(
     text_channel_id: str,
     team_role_id: str,
 ):
-    async with await psycopg.AsyncConnection.connect(DATABASE_URL) as aconn:
-        aconn = await psycopg.AsyncConnection.connect(DATABASE_URL)
-        acur = aconn.cursor()
+    aconn = await psycopg.AsyncConnection.connect(DATABASE_URL)
+    acur = aconn.cursor()
 
-        await acur.execute(
-            """
-            INSERT INTO public.teams
-            (team_name, category_channel_id, voice_channel_id, text_channel_id, team_role_id)
-            AS
-            VALUES(%s, %s, %s, %s, %s)
-            """,
-            (
-                team_name,
-                category_channel_id,
-                voice_channel_id,
-                text_channel_id,
-                team_role_id,
-            ),
-        )
+    await acur.execute(
+        """
+        INSERT INTO public.teams
+        (team_name, category_channel_id, voice_channel_id, text_channel_id, team_role_id)
+        AS
+        VALUES(%s, %s, %s, %s, %s)
+        """,
+        (
+            team_name,
+            category_channel_id,
+            voice_channel_id,
+            text_channel_id,
+            team_role_id,
+        ),
+    )
 
-        aconn.commit()
-        acur.close()
-        aconn.close()
+    aconn.commit()
+    acur.close()
+    aconn.close()
 
 
 async def get_team(team_name: str):
@@ -58,3 +57,25 @@ async def get_team(team_name: str):
     aconn.close()
 
     return result
+
+
+async def remove_team(team_name: str):
+    aconn = await psycopg.AsyncConnection.connect(DATABASE_URL)
+    acur = aconn.cursor(row_factory=class_row(Team))
+
+    # check existence of team
+    if not await get_team(team_name):
+        return False
+
+    # team exists otherwise
+    await acur.execute(
+        """
+        DELETE FROM public.teams 
+        WHERE t.team_name = %s
+        """,
+        (team_name,),
+    )
+
+    aconn.commit()
+    acur.close()
+    aconn.close()
