7c54a93cc830825c6d0f94df44e1606b414067a1
Adam Zhang <azha3852@uni.sydney.edu.au>
2024-02-28 12:47:51 +1100

    feat: added function for team invitation

--- cogs/team.py
+++ cogs/team.py
@@ -91,6 +91,60 @@ class Team(commands.GroupCog):
         # also delete the team
         team_query.remove_team(team_name)
 
+    @app_commands.command(name="invite")
+    async def invite(
+        self, interaction: discord.Interaction, invited_user: discord.Member
+    ):
+        user = interaction.user
+        guild = interaction.guild
+
+        # check user is already in a team
+        player = await player_query.get_player(user.id)
+        if not player:
+            interaction.response.send_message(
+                "You must be in a team to use this command."
+            )
+
+        team_name = player.team_name
+
+        # check invited user is not already in a team
+        if player_query.get_player(invited_user.id):
+            interaction.response.send_message(
+                "The user you're trying to invite is already in a team."
+            )
+
+        # otherwise send an invite
+        embed = discord.Embed(
+            title=f"{team_name} Invitation",
+            description=f"{user.name} has invited you to their team!",
+        )
+
+        accept_btn = discord.ui.Button(label="Accept", style=discord.ButtonStyle.green)
+        reject_btn = discord.ui.Button(label="Reject", style=discord.ButtonStyle.red)
+
+        view = discord.ui.View()
+        view.add_item(accept_btn)
+        view.add_item(reject_btn)
+
+        # add invited user to the team and database
+        async def accept_callback(interaction: discord.Interaction):
+            team = await team_query.get_team(team_name)
+            invited_user.add_roles(guild.get_role(int(team.team_role_id)))
+
+            # test line. delete later
+            print(interaction.user())
+
+        async def reject_callback(interaction: discord.Interaction):
+            await user.send(content="Your team invitation has been declined.")
+
+        accept_btn.callback = accept_callback
+        reject_btn.callback = reject_callback
+
+        await invited_user.send(embed=embed, view=view)
+        await interaction.response.send_message(
+            f"{invited_user} has been invited to your team.", ephemeral=True
+        )
+
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(Team(bot))
