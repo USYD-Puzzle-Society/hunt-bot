f6f286d7d9afd3e7a1b207e9eb9ed0ebee5911fe
Adam Zhang <adamzhang.aus@gmail.com>
2024-03-05 05:31:39 +1100

    feat: check if team has completed hunt after each submission

--- cogs/puzzle.py
+++ cogs/puzzle.py
@@ -7,12 +7,14 @@ import discord
 from discord.ext import commands
 from discord import app_commands
 
-from src.queries.puzzle import get_puzzle, create_puzzle
+from src.queries.puzzle import get_puzzle, get_completed_puzzles
 from src.queries.submission import (
     create_submission,
     find_submissions_by_player_id_and_puzzle_id,
 )
 from src.queries.player import get_player
+from src.queries.team import get_team, get_team_members
+
 from src.utils.decorators import in_team_channel
 from src.context.puzzle import can_access_puzzle, get_accessible_puzzles
 
@@ -56,6 +58,33 @@ class Puzzle(commands.GroupCog):
 
         if not submission_is_correct:
             return await interaction.followup.send("The submitted answer is incorrect!")
+
+        # check if they have solved all the metas
+        completed_puzzles = await get_completed_puzzles(player.team_name)
+        completed_ids = [puzz.puzzle_name for puzz in completed_puzzles]
+        if (
+            "UNSW-M" in completed_ids
+            and "USYD-M" in completed_ids
+            and "UTS-M" in completed_ids
+            and "METAMETA" in completed_ids
+        ):
+            team = await get_team(player.team_name)
+            await interaction.client.get_channel(config["ADMIN_CHANNEL_ID"]).send(
+                f"Team <#{team.text_channel_id}> has finished all the puzzles!"
+            )
+
+            # give team the victor role
+            guild = interaction.guild
+            team_members = await get_team_members(player.team_name)
+
+            for team_member in team_members:
+                discord_member = guild.get_member(team_member.discord_id)
+                discord_member.add_roles(guild.get_role(config["VICTOR_ROLE_ID"]))
+
+            await interaction.followup.send(
+                f"Congratulations! You've finished all the puzzles. Feel free to head over to the <#{config['VICTOR_TEXT_CHANNEL_ID']} in the Victory Lounge.>"
+            )
+            return
 
         await interaction.followup.send("The submitted answer is ...CORRECT!")
 
