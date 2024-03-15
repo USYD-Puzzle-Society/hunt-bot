a95b08c95c341b84e3a6a27ee49200240548b88c
Adam Zhang <adamzhang.aus@gmail.com>
2024-03-16 09:48:00 +1100

    fix: last minute hint time change

--- src/context/team.py
+++ src/context/team.py
@@ -83,7 +83,7 @@ async def remove_member_from_team(guild: discord.Guild, member: discord.Member):
 
 def get_max_hints():
     now = datetime.now(tz=ZoneInfo("Australia/Sydney"))
-    start = config["HUNT_START_TIME"]
+    start = config["HUNT_START_TIME"] + timedelta(hours=1)
 
     if now < start:
         return 0
@@ -117,7 +117,7 @@ async def check_if_max_hints(team_name: str):
 
 def get_next_hint_time() -> str:
     now = datetime.now(tz=ZoneInfo("Australia/Sydney"))
-    start = config["HUNT_START_TIME"]
+    start = config["HUNT_START_TIME"] + timedelta(hours=1)
 
     if now < start:
         return "10:15 16th of March"
